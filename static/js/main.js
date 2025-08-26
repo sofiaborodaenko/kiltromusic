00ed4a7a1ed32390f5d9d169ff1aff42f073ef3a
Sofia Borodaenko <soph.bor06@gmail.com>
2025-08-26 18:31:09 -0400

    added a different form of wide-block

--- static/js/main.js
+++ static/js/main.js
@@ -41,7 +41,7 @@ const revealSection = function (entries, observer) {
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.15,
+  threshold: 0.25,
 });
 
 allRevealSections.forEach(function (section) {
